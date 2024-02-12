c95b810e52cfe4f3921da667b039d1a88ec48390
Christopher Barrios The Man <68883351+InTimeTechnologies@users.noreply.github.com>
2024-02-12 15:08:19 -0500

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,71 +1,86 @@
 #pragma once
 
-// Forward declarations | Utility
+// Forward declarations
 template <typename T> class BinaryNode;
 template <typename T> class BinaryTree;
 
-// class BinaryNode 
+// class BinaryDirection
+enum class BinaryDirection {
+	NONE,
+	LEFT,
+	RIGHT
+};
+
+// class BinaryNode
 template <typename T> class BinaryNode {
 	// Friends
 	friend class BinaryTree<T>;
+
 	// Object
-	private:
-		// Properties
-		BinaryTree<T>* binaryTree;
-		BinaryNode<T>* parentNode;
-		BinaryNode<T>* leftNode;
-		BinaryNode<T>* rightNode;
 	public:
 		// Properties
 		T data;
 
+	private:
+		// Properties
+		BinaryNode<T>* parentNode = nullptr;
+		BinaryNode<T>* leftNode = nullptr;
+		BinaryNode<T>* rightNode = nullptr;
+		BinaryTree<T>* tree = nullptr;
+
+	public:
 		// Constructor / Destructor
 		BinaryNode();
-		BinaryNode(T data);
-		BinaryNode(const BinaryNode<T>& binaryNode);
-		void operator = (const BinaryNode<T>& binaryNode);
+		BinaryNode(const T& data);
 		~BinaryNode();
 
 		// Getters
-		BinaryTree<T>* getBinaryTree() const;
 		BinaryNode<T>* getParentNode() const;
 		BinaryNode<T>* getLeftNode() const;
 		BinaryNode<T>* getRightNode() const;
+		BinaryTree<T>* getTree() const;
 
 		// Functions
-		BinaryNode<T>* addLeftNode(T data);
-		BinaryNode<T>* addRightNode(T data);
-		BinaryNode<T>* getLeftmostLeaf() const;
-		BinaryNode<T>* getRightmostLeaf() const;
+		BinaryNode<T>* getRootNode() const;
+		BinaryNode<T>* getLeftmostNode() const;
+		BinaryNode<T>* getRightmostNode() const;
+		BinaryNode<T>* get(const T& data) const;
+		bool has(const T& data) const;
+		BinaryDirection getDirection() const;
 
-		void destroy();
+		BinaryNode<T>* addParentNode(const T& data, BinaryDirection parentNodeDirection);
+		BinaryNode<T>* addLeftNode(const T& data);
+		BinaryNode<T>* addRightNode(const T& data);
+		BinaryNode<T>* add(const T& data);
+		BinaryNode<T>* add(const T& data, BinaryDirection directionIfEqual);
+		bool erase(const T& data);
+		void clearNodes();
 };
 
 // class BinaryTree
 template <typename T> class BinaryTree {
 	// Friends
 	friend class BinaryNode<T>;
+
 	// Object
 	private:
 		// Properties
-		unsigned long long size;
-		BinaryNode<T>* rootNode;
+		int size = 0;
+		BinaryNode<T>* rootNode = nullptr;
+
 	public:
 		// Constructor / Destructor
 		BinaryTree();
-		BinaryTree(const BinaryTree<T>& binaryTree);
-		void operator = (const BinaryTree<T>& binaryTree);
+		BinaryTree(const BinaryTree<T>& tree);
+		void operator = (const BinaryTree<T>& tree);
 		~BinaryTree();
 
 		// Getters
-		unsigned long long getSize() const;
+		int getSize() const;
 		BinaryNode<T>* getRootNode() const;
-		BinaryNode<T>* getNodeWithData(T data) const;
 
 		// Functions
-		BinaryNode<T>* add(T data);
-		bool remove(T data);
-		bool has(T data);
+		BinaryNode<T>* add(const T& data);
 		void clear();
 };
 
@@ -74,19 +89,243 @@ template <typename T> class BinaryTree {
 // Object | public
 
 // Constructor / Destructor
-template <typename T> BinaryNode<T>::BinaryNode() : binaryTree(nullptr), parentNode(nullptr), leftNode(nullptr), rightNode(nullptr) {
-
+template <typename T> BinaryNode<T>::BinaryNode() {
+	
 }
-template <typename T> BinaryNode<T>::BinaryNode(T data) : binaryTree(nullptr), parentNode(nullptr), leftNode(nullptr), rightNode(nullptr), data(data) {
+template <typename T> BinaryNode<T>::BinaryNode(const T& data) : data(data) {
 
-}
-template <typename T> BinaryNode<T>::BinaryNode(const BinaryNode<T>& binaryNode) : binaryTree(nullptr), parentNode(nullptr), leftNode(nullptr), rightNode(nullptr), data(binaryNode.data) {
-
-}
-template <typename T> void BinaryNode<T>::operator = (const BinaryNode<T>& binaryNode) {
-	data = binaryNode.data;
 }
 template <typename T> BinaryNode<T>::~BinaryNode() {
+	clearNodes();
+
+	// Unlink this node from parent node
+	if (parentNode != nullptr) {
+		if (parentNode->leftNode == this)
+			parentNode->leftNode = nullptr;
+		else
+			parentNode->rightNode = nullptr;
+	}
+
+	if (tree != nullptr)
+		tree->size--;
+}
+
+// Getters
+template <typename T> BinaryNode<T>* BinaryNode<T>::getParentNode() const {
+	return parentNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::getLeftNode() const {
+	return leftNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::getRightNode() const {
+	return rightNode;
+}
+template <typename T> BinaryTree<T>* BinaryNode<T>::getTree() const {
+	return tree;
+}
+
+// Functions
+template <typename T> BinaryNode<T>* BinaryNode<T>::getRootNode() const {
+	if (parentNode == nullptr)
+		return nullptr;
+
+	BinaryNode<T>* currentNode = this;
+	while (currentNode->parentNode != nullptr)
+		currentNode = currentNode->parentNode;
+
+	return currentNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::getLeftmostNode() const {
+	if (leftNode == nullptr)
+		return nullptr;
+
+	BinaryNode<T>* currentNode = leftNode;
+	while (currentNode->leftNode != nullptr)
+		currentNode = leftNode->leftNode;
+
+	return currentNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::getRightmostNode() const {
+	if (rightNode == nullptr)
+		return nullptr;
+
+	BinaryNode<T>* currentNode = rightNode;
+	while (currentNode->rightNode != nullptr)
+		currentNode = rightNode->rightNode;
+
+	return currentNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::get(const T& data) const {
+	BinaryNode<T>* currentNode = this;
+	while (currentNode != nullptr) {
+		// If current node's data is equal to data provided, return current node
+		if (currentNode->data == data)
+			return currentNode;
+
+		// Check left branch
+		if (currentNode->leftNode != nullptr) {
+			currentNode = currentNode->leftNode;
+			continue;
+		}
+
+		// Traverse tree up searching for right nodes
+		while (currentNode->parentNode != nullptr) {
+			currentNode = currentNode->parentNode;
+			if (currentNode->rightNode != nullptr) {
+				// Trigger nodes's left branch check
+				currentNode = currentNode->rightNode;
+				break;
+			}
+		}
+	}
+
+	return nullptr;
+}
+template <typename T> bool BinaryNode<T>::has(const T& data) const {
+	return get(data) == nullptr ? false : true;
+}
+template <typename T> BinaryDirection BinaryNode<T>::getDirection() const {
+	if (parentNode == nullptr)
+		return BinaryDirection::NONE;
+
+	if (parentNode->leftNode == this)
+		return BinaryDirection::LEFT;
+	else if (parentNode->rightNode == this)
+		return BinaryDirection::RIGHT;
+
+	return BinaryDirection::NONE;
+}
+
+template <typename T> BinaryNode<T>* BinaryNode<T>::addParentNode(const T& data, BinaryDirection parentNodeDirection) {
+	// If this node already has a parent node, return false
+	if (parentNode != nullptr)
+		return nullptr;
+
+	// Link new node to this
+	BinaryNode<T>* newNode = new BinaryNode<T>();
+	newNode->data = data;
+	if (parentNodeDirection == BinaryDirection::LEFT)
+		newNode->rightNode = this;
+	else
+		newNode->leftNode = this;
+
+	// Edit tree
+	if (tree != nullptr) {
+		newNode->tree = tree;
+		tree->size++;
+		tree->rootNode = this;
+	}
+
+	// Link this to new node
+	parentNode = newNode;
+
+	// Return true
+	return newNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::addLeftNode(const T& data) {
+	// If this node already has a node to the left, return false
+	if (leftNode != nullptr)
+		return nullptr;
+
+	// Link new node to this
+	BinaryNode<T>* newNode = new BinaryNode<T>(data);
+	newNode->parentNode = this;
+	
+	// Edit tree
+	if (tree != nullptr) {
+		newNode->tree = tree;
+		tree->size++;
+	}
+
+	// Link this to new node
+	leftNode = newNode;
+
+	// Return true
+	return newNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::addRightNode(const T& data) {
+	// If this node already has a node to the right, return false
+	if (rightNode != nullptr)
+		return nullptr;
+
+	// Link new node to this
+	BinaryNode<T>* newNode = new BinaryNode<T>(data);
+	newNode->parentNode = this;
+
+	// Edit tree
+	if (tree != nullptr) {
+		newNode->tree = tree;
+		tree->size++;
+	}
+	
+	// Link this to new node
+	rightNode = newNode;
+
+	// Return true
+	return newNode;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::add(const T& data) {
+	BinaryNode<T>* newNode = new BinaryNode<T>(data);
+
+	BinaryNode<T>* currentNode = this;
+	while (currentNode != nullptr) {
+		if (data <= currentNode->data) {
+			if (currentNode->leftNode == nullptr)
+				return currentNode->addLeftNode(data);
+			
+			currentNode = currentNode->leftNode;
+			continue;
+		}
+		else if (data > currentNode->data) {
+			if (currentNode->rightNode == nullptr)
+				return currentNode->addRightNode(data);
+			
+			currentNode = currentNode->rightNode;
+			continue;
+		}
+
+		break;
+	}
+
+	// Failed to add to tree somehow
+	return nullptr;
+}
+template <typename T> BinaryNode<T>* BinaryNode<T>::add(const T& data, BinaryDirection directionIfEqual) {
+	BinaryNode<T>* newNode = new BinaryNode<T>(data);
+
+	BinaryNode<T>* currentNode = this;
+	while (currentNode != nullptr) {
+		if (data < currentNode->data || (data == currentNode->data && directionIfEqual == BinaryDirection::LEFT)) {
+			if (currentNode->leftNode == nullptr)
+				return currentNode->addLeftNode(data);
+
+			currentNode = currentNode->leftNode;
+			continue;
+		}
+		else if (data > currentNode->data || (data == currentNode->data && directionIfEqual == BinaryDirection::RIGHT)) {
+			if (currentNode->rightNode == nullptr)
+				return currentNode->addRightNode(data);
+
+			currentNode = currentNode->rightNode;
+			continue;
+		}
+
+		break;
+	}
+
+	// Failed to add to tree somehow
+	return nullptr;
+}
+template <typename T> bool BinaryNode<T>::erase(const T& data) {
+	BinaryNode<T>* nodeToErase = get(data);
+	
+	if (nodeToErase == nullptr)
+		return false;
+
+	delete(nodeToErase);
+	return true;
+}
+template <typename T> void BinaryNode<T>::clearNodes() {
 	// Start destruction on left side
 	BinaryNode<T>* currentNode = leftNode;
 
@@ -115,83 +354,6 @@ template <typename T> BinaryNode<T>::~BinaryNode() {
 		if (currentNode == this)
 			currentNode = currentNode->rightNode;
 	}
-
-	// Unlink this node from parent node
-	if (parentNode != nullptr) {
-		if (parentNode->leftNode == this)
-			parentNode->leftNode = nullptr;
-		else
-			parentNode->rightNode = nullptr;
-	}
-
-	if (binaryTree != nullptr) {
-		// Decrement tree size
-		binaryTree->size--;
-
-		// If this is tree's root node, assign it to nullptr
-		if (this == binaryTree->rootNode)
-			binaryTree->rootNode = nullptr;
-	}
-}
-
-// Getters
-template <typename T> BinaryTree<T>* BinaryNode<T>::getBinaryTree()  const {
-	return binaryTree;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::getParentNode()  const {
-	return parentNode;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::getLeftNode()  const {
-	return leftNode;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::getRightNode()  const {
-	return rightNode;
-}
-
-// Functions
-template <typename T> BinaryNode<T>* BinaryNode<T>::addLeftNode(T data) {
-	if (leftNode != nullptr)
-		return nullptr;
-
-	leftNode = new BinaryNode<T>(data);
-	leftNode->binaryTree = binaryTree;
-	leftNode->parentNode = this;
-
-	return leftNode;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::addRightNode(T data) {
-	if (rightNode != nullptr)
-		return nullptr;
-
-	rightNode = new BinaryNode<T>(data);
-	rightNode->binaryTree = binaryTree;
-	rightNode->parentNode = this;
-
-	return rightNode;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::getLeftmostLeaf() const{
-	if (leftNode == nullptr)
-		return nullptr;
-
-	BinaryNode<T>* currentNode = leftNode;
-	while (currentNode->leftNode != nullptr)
-		currentNode = leftNode->leftNode;
-
-	return currentNode;
-}
-template <typename T> BinaryNode<T>* BinaryNode<T>::getRightmostLeaf() const {
-	if (rightNode == nullptr)
-		return nullptr;
-
-	BinaryNode<T>* currentNode = rightNode;
-	while (currentNode != nullptr)
-		currentNode = currentNode->rightNode;
-
-	return currentNode;
-}
-
-template <typename T> void BinaryNode<T>::destroy() {
-	delete(this);
 }
 
 // class BinaryTree
@@ -199,16 +361,16 @@ template <typename T> void BinaryNode<T>::destroy() {
 // Object | public
 
 // Constructor / Destructor
-template <typename T> BinaryTree<T>::BinaryTree() : size(0ULL), rootNode(nullptr) {
+template <typename T> BinaryTree<T>::BinaryTree() {
 
 }
-template <typename T> BinaryTree<T>::BinaryTree(const BinaryTree<T>& binaryTree) {
+template <typename T> BinaryTree<T>::BinaryTree(const BinaryTree<T>& tree) {
 	// If tree is empty, return false
-	if (binaryTree.size == 0ULL)
+	if (tree.size == 0ULL)
 		return;
 
 	// Search tree for data
-	BinaryNode<T>* otherCurrentNode = binaryTree.rootNode;
+	BinaryNode<T>* otherCurrentNode = tree.rootNode;
 	BinaryNode<T>* thisCurrentNode = new BinaryNode<T>(this, otherCurrentNode->data);
 	while (otherCurrentNode != nullptr) {
 		// Check left branch
@@ -230,16 +392,16 @@ template <typename T> BinaryTree<T>::BinaryTree(const BinaryTree<T>& binaryTree)
 		}
 	}
 }
-template <typename T> void BinaryTree<T>::operator = (const BinaryTree<T>& binaryTree) {
+template <typename T> void BinaryTree<T>::operator = (const BinaryTree<T>& tree) {
 	// Clear this tree of nodes
 	clear();
 
 	// If tree is empty, return false
-	if (binaryTree.size == 0ULL)
+	if (tree.size == 0ULL)
 		return;
 
 	// Search tree for data
-	BinaryNode<T>* otherCurrentNode = binaryTree.rootNode;
+	BinaryNode<T>* otherCurrentNode = tree.rootNode;
 	BinaryNode<T>* thisCurrentNode = new BinaryNode<T>(this, otherCurrentNode->data);
 	while (otherCurrentNode != nullptr) {
 		// Check left branch
@@ -266,163 +428,23 @@ template <typename T> BinaryTree<T>::~BinaryTree() {
 }
 
 // Getters
-template <typename T> unsigned long long BinaryTree<T>::getSize() const {
+template <typename T> int BinaryTree<T>::getSize() const {
 	return size;
 }
 template <typename T> BinaryNode<T>* BinaryTree<T>::getRootNode() const {
 	return rootNode;
 }
-template <typename T> BinaryNode<T>* BinaryTree<T>::getNodeWithData(T data) const {
-	// If tree is empty, return nullptr
-	if (rootNode == nullptr)
-		return nullptr;
-
-	// Check root node
-	if (rootNode->data == data)
-		return rootNode;
-
-	// Sentinel value
-	bool visitedRootNode = false;
-
-	// Start search on left side
-	BinaryNode<T>* currentNode = rootNode->leftNode;
-
-	while (currentNode != nullptr) {
-		// If data found, return current node
-		if (currentNode->data == data)
-			return currentNode;
-
-		// Search for leaf
-		if (currentNode->leftNode != nullptr) {
-			currentNode = currentNode->leftNode;
-			continue;
-		}
-		if (currentNode->rightNode != nullptr) {
-			currentNode = currentNode->rightNode;
-			continue;
-		}
-
-		// If all nodes visisted, break from search / loop
-		if (visitedRootNode)
-			break;
-
-		// Start search on left side
-		BinaryNode<T>* parentNode = currentNode->parentNode;
-		if (parentNode == rootNode) {
-			visitedRootNode = true;
-			currentNode = rootNode->rightNode;
-		}
-	}
-
-	// Return nullptr
-	return nullptr;
-}
 
 // Functions
-template <typename T> BinaryNode<T>* BinaryTree<T>::add(T data) {
-	// If tree is empty, add to tree as root node
-	if (size == 0ULL) {
+template <typename T> BinaryNode<T>* BinaryTree<T>::add(const T& data) {
+	if (size == 0) {
 		rootNode = new BinaryNode<T>(data);
-		rootNode->binaryTree = this;
+		rootNode->tree = this;
+		size++;
 		return rootNode;
 	}
 
-
-	// Add to tree based on data value (greater or equal)
-	BinaryNode<T>* currentNode = rootNode;
-	while (currentNode != nullptr) {
-		if (data <= currentNode->data) {
-			if (currentNode->leftNode == nullptr)
-				return currentNode->addLeftNode(data);
-
-			currentNode = currentNode->leftNode;
-			continue;
-		}
-		if (data > currentNode->data) {
-			if (currentNode->rightNode == nullptr)
-				return currentNode->addRightNode(data);
-
-			currentNode = currentNode->rightNode;
-			continue;
-		}
-		break;
-	}
-
-	// Failed to add to tree somehow
-	return nullptr;
-}
-template <typename T> bool BinaryTree<T>::remove(T data) {
-	// If tree is empty, return false
-	if (size == 0ULL)
-		return false;
-
-	// Check root node
-	if (rootNode->data == data) {
-		delete(rootNode);
-		return true;
-	}
-
-	// Search tree for data
-	BinaryNode<T>* currentNode = rootNode;
-	while (currentNode != nullptr) {
-		// If data found, return true
-		if (currentNode->data == data) {
-			delete(currentNode);
-			return true;
-		}
-
-		// Check left branch
-		if (currentNode->leftNode != nullptr) {
-			currentNode = currentNode->leftNode;
-			continue;
-		}
-
-		// Traverse tree upwards searching for right nodes
-		while (currentNode->parentNode != nullptr) {
-			currentNode = currentNode->parentNode;
-			if (currentNode->rightNode != nullptr) {
-				// Trigger right node's left branch check
-				currentNode = currentNode->rightNode;
-				break;
-			}
-		}
-	}
-}
-template <typename T> bool BinaryTree<T>::has(T data) {
-	// If tree is empty, return false
-	if (size == 0ULL)
-		return false;
-
-	// Check root node
-	if (rootNode->data == data)
-		return true;
-
-	// Search tree for data
-	BinaryNode<T>* currentNode = rootNode;
-	while (currentNode != nullptr) {
-		// If data found, return true
-		if (currentNode->data == data)
-			return true;
-
-		// Check left branch
-		if (currentNode->leftNode != nullptr) {
-			currentNode = currentNode->leftNode;
-			continue;
-		}
-
-		// Traverse tree upwards searching for right nodes
-		while (currentNode->parentNode != nullptr) {
-			currentNode = currentNode->parentNode;
-			if (currentNode->rightNode != nullptr) {
-				// Trigger right node's left branch check
-				currentNode = currentNode->rightNode;
-				break;
-			}
-		}
-	}
-
-	// Data not found in tree
-	return false;
+	return rootNode->add(data);
 }
 template <typename T> void BinaryTree<T>::clear() {
 	delete(rootNode);
